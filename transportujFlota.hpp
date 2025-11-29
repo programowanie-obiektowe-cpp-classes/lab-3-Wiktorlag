96f987665af62bef1fc3f76382699044fd8e243e
Wiktor Łagoszyn <wiktorlag2003@wp.pl>
2025-11-29 13:32:05 +0100

    29.11

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -5,5 +5,18 @@
 unsigned int transportujFlota(unsigned int towar)
 {
     // Twoja implementacja tutaj
-    return 0;
+    int suma_przetransportowanego_towaru = 0;
+    int liczbaZaglowcow = 0;
+    Stocznia stocznia{};
+    while (!(suma_przetransportowanego_towaru >= towar)) 
+        {
+        Statek*  s1 = stocznia();
+        int transportowanytowar = s1->transportuj();
+        suma_przetransportowanego_towaru += transportowanytowar;
+        if (dynamic_cast< Zaglowiec* >(s1) != nullptr) {
+            liczbaZaglowcow++;
+        }
+        delete s1;
+    }
+return liczbaZaglowcow;
 }
